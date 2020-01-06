59fb01250775b2ff4d16a2679b90d48540bce652
zhanweichun <zhanweichun@sittone.com>
2020-01-06 14:36:17 +0800

    1.0

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/zhanweichun/Documents/soft/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/zhanweichun/Documents/work/code/flutter/flutter_shuqi"
+export "FLUTTER_TARGET=/Users/zhanweichun/Documents/work/code/flutter/flutter_shuqi/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/zhanweichun/Documents/soft/dev/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
